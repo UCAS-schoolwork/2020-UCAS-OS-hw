d733ea26d7823f7b01136c4de807d81f48f84d5d
cyf <54574620+ChenLeapin@users.noreply.github.com>
2021-03-06 17:08:35 +0800

    Add files via upload

---
+++ hw1/addr_space.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *myname="Bao Yungang";
+char gdata[128];
+char bdata[16] = {1,2,3,4};
+main() {
+	char * ldata[16];	
+	char * ddata;
+
+	ddata = malloc(16);
+	printf("gdata: %llX\nbdata:%llX\nldata:%llx\nddata:%llx\n",
+		gdata,bdata,ldata,ddata);
+	free(ddata);
+}
